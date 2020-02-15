c634cbf84604e977c4cb57485a14f752dd0d656b
Patrick Adesso <padesso@deximaging.com>
2020-02-14 21:35:16 -0500

    Basics of benchmarking in place.

---
+++ EconomySim/EconomySimConsoleBenchmarks/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomySimConsoleBenchmarks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IterationTests tests = new IterationTests();
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+
+        [HtmlExporter]
+        public class IterationTests
+        {
+            [Params(10, 100, 1000, 10000, 100000, 1000000, 1000000000)]
+            public int Iterations { get; set; }
+
+            [Benchmark]
+            public void IterationTest()
+            {
+                for (int i = 0; i < Iterations; i++)
+                {
+                    int j = i;
+                }
+            }
+        }
+    }
+}
